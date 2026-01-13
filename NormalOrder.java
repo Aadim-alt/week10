c5d640525e97fd4661cde504f01422800a9f6595
Aadim-alt <aadimdhakal8@gmail.com>
2026-01-13 13:51:55 +0545

    Add NormalOrder class extending Order

---
+++ NormalOrder.java
@@ -0,0 +1,13 @@
+public class NormalOrder extends Order {
+
+    public NormalOrder(int orderId, String customerName, double amount) {
+        super(orderId, customerName, amount);
+    }
+
+    
+    @Override
+    public double calculateFinalAmount() {
+        
+        return super.calculateFinalAmount();
+    }
+}
